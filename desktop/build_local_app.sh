c55fde8d0c44001efbf69453f25aa934f56c3890
Daniel Goodwyn <danielgoodwyn@gmail.com>
2026-06-19 13:58:49 -0700

    Add generated desktop app icon

--- desktop/build_local_app.sh
+++ desktop/build_local_app.sh
@@ -5,11 +5,32 @@ APP_NAME="Local Image Generator"
 ROOT="/Users/danielgoodwyn/src/Local Image Generator"
 DESKTOP_DIR="$ROOT/desktop"
 BUILD_DIR="$DESKTOP_DIR/build"
+ICON_SOURCE="$DESKTOP_DIR/assets/local-image-generator-app-icon.png"
+ICONSET="$BUILD_DIR/LocalImageGenerator.iconset"
+ICON_FILE="$BUILD_DIR/LocalImageGenerator.icns"
 APP_BUNDLE="$BUILD_DIR/$APP_NAME.app"
 INSTALL_BUNDLE="/Users/danielgoodwyn/Applications/$APP_NAME.app"
 DESKTOP_LINK="/Users/danielgoodwyn/Desktop/$APP_NAME.app"
 
+rm -rf "$APP_BUNDLE"
 mkdir -p "$APP_BUNDLE/Contents/MacOS" "$APP_BUNDLE/Contents/Resources"
+
+if [ -f "$ICON_SOURCE" ]; then
+  rm -rf "$ICONSET"
+  mkdir -p "$ICONSET"
+  sips -z 16 16 "$ICON_SOURCE" --out "$ICONSET/icon_16x16.png" >/dev/null
+  sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET/icon_16x16@2x.png" >/dev/null
+  sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET/icon_32x32.png" >/dev/null
+  sips -z 64 64 "$ICON_SOURCE" --out "$ICONSET/icon_32x32@2x.png" >/dev/null
+  sips -z 128 128 "$ICON_SOURCE" --out "$ICONSET/icon_128x128.png" >/dev/null
+  sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET/icon_128x128@2x.png" >/dev/null
+  sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET/icon_256x256.png" >/dev/null
+  sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET/icon_256x256@2x.png" >/dev/null
+  sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET/icon_512x512.png" >/dev/null
+  sips -z 1024 1024 "$ICON_SOURCE" --out "$ICONSET/icon_512x512@2x.png" >/dev/null
+  iconutil -c icns "$ICONSET" -o "$ICON_FILE"
+  cp "$ICON_FILE" "$APP_BUNDLE/Contents/Resources/LocalImageGenerator.icns"
+fi
 
 swiftc "$DESKTOP_DIR/LocalImageGeneratorApp.swift" \
   -o "$APP_BUNDLE/Contents/MacOS/LocalImageGenerator" \
@@ -25,6 +46,8 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<'PLIST'
   <key>CFBundleDisplayName</key>
   <string>Local Image Generator</string>
   <key>CFBundleExecutable</key>
+  <string>LocalImageGenerator</string>
+  <key>CFBundleIconFile</key>
   <string>LocalImageGenerator</string>
   <key>CFBundleIdentifier</key>
   <string>com.danielgoodwyn.local-image-generator</string>
